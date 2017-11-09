0cd9fc87088613852eef6a861a99abb0dc46c6d9
Yahniukov <yahniukov.work@gmail.com>
2017-11-09 19:51:38 +0200

    Update reg_sync_reset

---
+++ projects/reg_sync_reset/src/design/d_trig.v
@@ -0,0 +1,29 @@
+module d_trig(
+    D, C, R,    // in
+    Q, notQ     // out
+    );
+
+    // Parameters
+    parameter INIT_VAL    = 0;
+    parameter RESET_VAL   = 1;
+
+    // Ports
+    input wire D, C, R;
+    output wire Q, notQ;
+
+    // Variables
+    reg Q_reg = INIT_VAL;
+
+    // Behavioral
+
+    always @(posedge C) begin
+        if(R == RESET_VAL)
+            Q_reg <= INIT_VAL;
+        else
+            Q_reg <= D;
+    end
+
+    assign Q    = Q_reg;
+    assign notQ = !Q_reg;
+
+endmodule
